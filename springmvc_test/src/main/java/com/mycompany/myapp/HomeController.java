5e76da4df05efa39f7070cae2acd2fab9d28254a
sjrnfl23 <tnsgud1010@m365.dongyang.ac.kr>
2022-02-22 13:59:07 +0900

    2

--- springmvc_test/src/main/java/com/mycompany/myapp/HomeController.java
+++ springmvc_test/src/main/java/com/mycompany/myapp/HomeController.java
@@ -1,4 +1,4 @@
-package com.mycompany.myapp.durian;
+package com.mycompany.myapp;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -11,21 +11,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.mycompany.myapp.HomeController;
-
 /**
  * Handles requests for the application home page.
  */
-
 @Controller
-public class DurianController {
+public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "durian/memberview", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -36,8 +33,20 @@ public class DurianController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "durian/memberview";
+		return "home";
 	}
-
+	@RequestMapping(value = "/wonyoung", method = RequestMethod.GET)
+	public String wonyoung(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "wonyoung";
+	}
 	
 }
